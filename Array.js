8efc663f20fc623cb2a8704154da816c11243f76
Gianmaria Canossa <g.canossa@aliaslab.net>
2017-04-11 13:18:38 +0200

    Committer: Gianmaria Canossa <g.canossa@aliaslab.net> On branch master

---
+++ Array.js
@@ -0,0 +1,279 @@
+﻿Array.extensions = {};
+
+Array.extensions.remove = function (item, comparator) {
+    var cmp = null;
+    if (comparator && (typeof (comparator)).toLowerCase() == "function") {
+        cmp = function (a, b) {
+            return comparator(a, b);
+        };
+    }
+    else {
+        cmp = function (a, b) {
+            return a == b;
+        }
+    }
+
+    for (var i = 0; i < this.length; i++)
+        if (cmp(this[i], item)) {
+            this.splice(i, 1);
+            break;
+        }
+};
+
+Array.extensions.addUnique = function (item, comparator) {
+    var cmp = null;
+    if (comparator && (typeof (comparator)).toLowerCase() == "function") {
+        cmp = function (a, b) {
+            return comparator(a, b);
+        };
+    }
+    else {
+        cmp = function (a, b) {
+            return a == b;
+        }
+    }
+
+    for (var i = 0; i < this.length; i++)
+        if (cmp(this[i], item))
+            return false;
+
+    this.push(item);
+    return true;
+}
+
+Array.extensions.contains = function (item, comparator) {
+    var cmp = null;
+    if (comparator && (typeof (comparator)).toLowerCase() == "function") {
+        cmp = function (a, b) {
+            return comparator(a, b);
+        };
+    }
+    else {
+        cmp = function (a, b) {
+            return a == b;
+        }
+    }
+
+    for (var i = 0; i < this.length; i++)
+        if (cmp(this[i], item))
+            return true;
+    return false;
+}
+
+Array.extensions.selectOne = function (item, comparator) {
+    var cmp = null;
+    if (comparator && (typeof (comparator)).toLowerCase() == "function") {
+        cmp = function (a, b) {
+            return comparator(a, b);
+        };
+    }
+    else {
+        cmp = function (a, b) {
+            return a == b;
+        }
+    }
+
+    for (var i = 0; i < this.length; i++)
+        if (cmp(this[i], item))
+            return this[i];
+    return null;
+}
+
+Array.extensions.where = function (selector) {
+    selector = selector || function () { return true; };
+    var sel = null;
+    if (selector && (typeof (selector)).toLowerCase() == "function") {
+        sel = function (a) {
+            return selector(a);
+        };
+    }
+    else {
+        sel = function (a) {
+            return true;
+        }
+    }
+
+    var list = [];
+    for (var i = 0; i < this.length; i++)
+        if (sel(this[i]))
+            list.push(this[i]);
+    return list;
+}
+
+Array.extensions.count = function (selector) {
+    selector = selector || function () { return true; };
+    return this.where(selector).length;
+}
+
+Array.extensions.first = function (selector) {
+    selector = selector || function () { return true; };
+    var tmp=this.where(selector);
+    return tmp.length>0?tmp[0]:null;
+}
+
+Array.extensions.last = function (selector) {
+    selector = selector || function () { return true; };
+    var tmp = this.where(selector);
+    return tmp.length > 0 ? tmp[tmp.length-1] : null;
+}
+
+Array.extensions.select = function (transformer) {
+    var list = [];
+    transformer = transformer || function (a) { return a; };
+    for (var i = 0; i < this.length; i++)
+        list.push(transformer(this[i]));
+    return list;
+}
+
+Array.extensions.next = function (item, comparator, rotate) {
+    var cmp = null;
+    if (comparator && (typeof (comparator)).toLowerCase() == "function") {
+        cmp = function (a, b) {
+            return comparator(a, b);
+        };
+    }
+    else {
+        cmp = function (a, b) {
+            return a == b;
+        }
+    }
+
+    for (var i = 0; i < this.length; i++)
+        if (cmp(this[i], item))
+            break;
+
+    if (i == this.length)
+        return null;
+    else if (i == this.length - 1)
+        if (rotate)
+            return this[0];
+        else
+            return null;
+    else
+        return this[i + 1];
+}
+
+Array.extensions.prev = function (item, comparator, rotate) {
+    var cmp = null;
+    if (comparator && (typeof (comparator)).toLowerCase() == "function") {
+        cmp = function (a, b) {
+            return comparator(a, b);
+        };
+    }
+    else {
+        cmp = function (a, b) {
+            return a == b;
+        }
+    }
+
+    for (var i = 0; i < this.length; i++)
+        if (cmp(this[i], item))
+            break;
+
+    if (i == this.length)
+        return null;
+    else if (i == 0)
+        if (rotate)
+            return this[this.length-1];
+        else
+            return null;
+    else
+        return this[i - 1];
+}
+
+Array.extensions.Union = function (list, comparator) {
+    var tmp = this.slice(0);
+    for (var i = 0; i < list.length; i++)
+        tmp.addUnique(list[i], comparator);
+    return tmp;
+}
+
+Array.extensions.Except = function (list, comparator) {
+    var tmp = this.slice(0);
+    for (var i = 0; i < list.length; i++)
+        tmp.remove(list[i], comparator);
+    return tmp;
+}
+
+Array.extensions.Intersect = function (list, comparator) {
+    return this.Except(this.Except(list, comparator), comparator);
+}
+
+Array.extensions.groupBy = function (comparator,namer) {
+    var res = [];
+    for (var i = 0; i < this.length; i++) {
+        var tmp = null;
+        for (var j = 0; j < res.length; j++) {
+            if (res[j].items.contains(this[i], comparator)) {
+                tmp = res[j];
+                break;
+            }
+        }
+        if (tmp == null) {
+            var el = this[i];
+            res.push({items:[el]});
+        }
+        else {
+            tmp.items.push(this[i]);
+        }
+    }
+    if (namer && (typeof (namer)).toLowerCase() == "function") {
+        for (var i = 0; i < res.length; i++)
+            res[i].name = namer(res[i]);
+    }
+    return res;
+}
+
+Array.extensions.orderBy = function (comparator) {
+    var res = [];
+    for (var i = 0; i < this.length; i++) {
+        var j = 0;
+        while (j < res.length && comparator(this[i], res[j]) >= 0) j++;
+        res.splice(j,0,this[i]);
+    }
+    return res;
+}
+
+Array.extensions.distinct = function (comparator) {
+    var res = [];
+    for (var i = 0; i < this.length; i++) {
+        if (!res.contains(this[i], comparator))
+            res.push(this[i]);
+    }
+    return res;
+}
+
+Array.extensions.any = function (selector) {
+    selector = selector || function () { return true; };
+    var tmp = this.where(selector);
+    return tmp.length > 0;
+}
+
+Array.extensions.selectMany = function (transformer) {
+    var list = [];
+    transformer = transformer || function (a) { return a; };
+    for (var i = 0; i < this.length; i++) {
+        var tmp = transformer(this[i]);
+        for (var j = 0; j < tmp.length;j++)
+            list.push(tmp[j]);
+    }
+    return list;
+}
+
+Array.extensions.insertOrdered = function (item, cmp) {
+    var list = [];
+    cmp = cmp || function (a, b) { return 1; };
+    var i = 0;
+    for (; i < this.length; i++) {
+        if (cmp(item, this[i]) <= 0) {
+            break;
+        }
+    }
+
+    this.splice(i,0,item);
+
+    return this;
+}
+
+for (var name in Array.extensions)
+    Array.prototype[name] = Array.extensions[name];
--- README.md
+++ README.md
@@ -1,2 +1,97 @@
 # Array
 Library which adds to the array object query capabilities LINQ-like.
+
+When loaded add to the javascript Array object the methods:
+
+- **remove(item[, comparator]):void**: removes an item from the array
+- **addUnique(item[,comparator]):bool**: add an item to the arry if not present
+- **contains(item[, comparator]):bool**: true if the array contains the element
+- **selectOne(item[, comparator]):object**: select the item element if matches the comparison
+- **where(selector):array**: selects a portion of the array
+- **count(selector):int**: counts the element in a selection
+- **first(selector):object**: reuturn the first matching element
+- **last(selector):object**: returns the last mathcing element
+- **select(transformer):object**: projects a protion of the array object in another array
+- **next(item[, comparator, rotate]):object**: obtains the element succeding to the provided one, optionally rotating in the array.
+- **prev(item[, comparator, rotate]):object**:obtains the element preceding to the provided one, optionally rotating in the array.
+- **Union(list[, comparator]):array**: returs a union of the current array with the one provided.
+- **Except(list[, comparator]):array**: return the differnece between the two array.
+- **Intersect(list[, comparator]):array**: return the intersection between the two array.
+- **groupBy(comparator, namer):array**: returns an array of groups. Every group has the attribute _name_ set by the namer funcion and the attribute _items_ containing the grouped elements.
+- **orderBy(comparator):array**: orders the array
+- **distinct([comparator]):self**: gets an array without duplicates.
+- **any(selector):bool**: returns true if at least one element matches the condition.
+- **selectMany(merger):array**: returns an array fusing the arry returned by the _merger_ function
+- **insertOrdered(item[, comparator]):self**: insert an element keeping the order in the array
+
+### Function signatures
+
+**comparator(a,b)** 
+
+must return true if the a==b. Default control is a==b
+
+**selector(p)**
+
+must return true if the condition evaluated on the element is matched
+
+**transformer(p)**
+
+returns an object obtained from the element
+
+**merger(p)**
+
+must return an array, obtained from the element
+
+#Example
+
+<pre>
+
+var test=[
+	{
+		name:"one",
+		values:[1,11,111]
+	},
+	{
+		name:"two",
+		values:[2,22,222]
+	},
+	{
+		name:"three",
+		values:[3,33,333]
+	}
+];
+
+</pre>
+
+<pre>
+test.selectMany(function(p){ return p.values}); //[1,11,111,2,22,222,3,33,333]
+</pre>
+<pre>
+test.selectMany(function(p){ return p.values}).where(function(p){return p<10}); //[1,2,3]
+</pre>
+
+<pre>
+test.groupBy(function(a,b){ return a.name[0]==b.name[0];},funcion(p){return p.items.first().name[0];})
+/*	result
+	[
+		{
+			name:"o", 
+			items:[{
+				name:"one",
+				values:[1,11,111]
+			}]
+		},{
+			name:"t", 
+			items:[
+			{
+				name:"two",
+				values:[2,22,222]
+			},
+			{
+				name:"three",
+				values:[3,33,333]
+			}]
+		}
+	]
+*/
+</pre>
